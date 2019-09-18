42211b6e0f1639baf15cda2a44548f42bbe66a48
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-19 00:51:58 +0800

    登录-验证完成

--- src/main.js
+++ src/main.js
@@ -3,20 +3,52 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
+import * as rules from 'vee-validate/dist/rules'
+// 这种加载没有携带任何内置的验证规则
+// import { ValidationProvider, extend } from 'vee-validate'
+
+// 如果需要使用其内置的验证规则,则要使用这种方式验证
+import { ValidationProvider, extend, ValidationObserver } from 'vee-validate/dist/vee-validate.full'
 
 import {// 按需注册vant组件
   Button,
   Cell,
   CellGroup,
   NavBar,
-  Field
+  Field,
+  Notify,
+  Toast
 } from 'vant'
+
 Vue
   .use(Button)
   .use(Cell)
   .use(CellGroup)
   .use(NavBar)
   .use(Field)
+  .use(Notify)
+  .use(Toast)
+
+// 配置表单验证插件
+// 配置使用中文语言
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+
+extend('phone', {
+  validate (value) {
+    return /^1\d{10}$/.test(value)
+  }, // the validation function
+  message: '必须是手机号' // the error message
+})
+
+// Register it globally
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="login">
-      <!-- 导航栏 -->
+    <!-- 导航栏 -->
     <van-nav-bar title="登录" />
 
     <!-- 登录表单 -->
     <van-cell-group>
-  <van-field
-  v-model="user.mobile"
-    required
-    clearable
-    label="手机号"
-    placeholder="请输入手机号"
-  />
-
-  <van-field
-   v-model="user.code"
-    type="password"
-    label="验证码"
-    placeholder="请输入验证码"
-    required
-  />
-</van-cell-group>
-<!-- 登录按钮 -->
-<div class="login-btn">
-    <van-button :loading="isLoginLoading" @click="onLogin" type="info">登录</van-button>
-</div>
+        <!-- ValidationObserver会渲染成一个form表单
+        可以通过ref调用这个组件的方法,validate来进行js验证 -->
+      <ValidationObserver tag="form" ref="loginForm">
+  <ValidationProvider tag="div" name="手机号" rules="required|phone" v-slot="{ errors }">
+    <van-field
+      v-model="user.mobile"
+      clearable
+      label="手机号"
+      placeholder="请输入手机号"
+      :error-message="errors[0]"
+    />
+  </ValidationProvider>
+  <ValidationProvider tag="div" name="验证码" rules="required" v-slot="{ errors }">
+    <van-field
+      v-model="user.code"
+      type="password"
+      label="验证码"
+      placeholder="请输入验证码"
+      :error-message="errors[0]"
+    />
+  </ValidationProvider>
+</ValidationObserver>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="login-btn">
+      <van-button :loading="isLoginLoading" @click="onLogin" type="info">登录</van-button>
+    </div>
+    <!-- validationProvider是验证插件提供的一个全局组件
+    rules="secret"配置验证规则
+    v-slot="{errors}"获取验证相关的结果参数
+    errors[0]用来获取验证失败的错误消息-->
+    <!-- <ValidationProvider name="first name" rules="required|min:2" v-slot="{ errors }">
+  <input v-model="value" type="text">
+  <span>{{ errors[0] }}</span>
+    </ValidationProvider>-->
   </div>
 </template>
 
@@ -34,19 +49,30 @@ export default {
   name: 'LoginIndex',
   data () {
     return {
+      email: '123',
       user: {
-        mobile: '13111111111',
-        code: '246810'
+        mobile: '',
+        code: ''
       },
-      isLoginLoading: false// 防止用户频繁点击
+      isLoginLoading: false // 防止用户频繁点击
     }
   },
   methods: {
     async onLogin () {
-      this.isLoginLoading = true
       try {
+        //   表单验证
+        const isValid = await this.$refs.loginForm.validate()
+
+        // 如果验证失败则什么都不做
+        if (!isValid) {
+          return
+        }
+
+        // 验证通过提交表单
+        this.isLoginLoading = true
         const { data } = await login(this.user)
         console.log(data)
+        this.$toast.success('登陆成功')
       } catch (err) {
         if (err.response && err.response.status === 400) {
           this.$toast.fail('登陆失败,用户名或密码错误')
@@ -59,12 +85,12 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .login {
-        .login-btn{
-        padding: 20px;
-        .van-button {
-            width: 100%;
-        }
+.login {
+  .login-btn {
+    padding: 20px;
+    .van-button {
+      width: 100%;
     }
-    }
+  }
+}
 </style>
