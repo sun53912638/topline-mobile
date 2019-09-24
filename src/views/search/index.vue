694b669d5249a2ba421ccd3f7f62582dd7afdb8a
sun53912638 <53866851+sun53912638@users.noreply.github.com>
2019-09-24 19:32:55 +0800

    搜索-搜索联想建议

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -15,8 +15,13 @@
 
     <!-- 联想建议 -->
     <van-cell-group>
-      <van-cell icon="search" v-for="item in 5" :key="item">
-        <div slot="title">{{item}}</div>
+      <van-cell
+      icon="search"
+      v-for="item in suggestions"
+      :key="item"
+      @click="onSearch(item)"
+      >
+        <div slot="title" v-html="item"></div>
       </van-cell>
     </van-cell-group>
 
@@ -38,14 +43,15 @@
 </template>
 
 <script>
-// import { getSearchSuggestions } from '@/api/search'
+import { getSearchSuggestions } from '@/api/search'
 
 export default {
   name: 'searchText',
 
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      suggestions: []
     }
   },
 
@@ -55,14 +61,17 @@ export default {
   },
 
   watch: {
-    async serachText (newValue) {
+    // 封装接口,监视搜索文本的变化 -> 发送请求 -> 获取结果 -> 将结果绑定到数据 -> 模板绑定
+    async searchText (newValue) {
       // 校验非空
       if (!newValue.length) {
-
+        return
       }
 
       //   请求获取数据
-    //   const { data } = await getSearchSuggestions()
+      const { data } = await getSearchSuggestions(newValue)
+      console.log(data)
+      this.suggestions = data.data.options
     }
   }
 }
